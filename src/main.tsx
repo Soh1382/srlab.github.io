270c64b23357436b03938165bd073c23434bc727
Soh1382 <soheil1382@gmail.com>
2025-12-22 23:30:17 +0000

    fix the console error

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import { Buffer } from 'buffer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(globalThis as any).Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
