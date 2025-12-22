15d7ed9d51c27faf2c3b89da2616abb563a7cc11
Soh1382 <soheil1382@gmail.com>
2025-12-22 23:15:04 +0000

    replace browser router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ScrollToTop from './components/ScrollToTop';
 import AllBlogs from './pages/AllBlogs';
